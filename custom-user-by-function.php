640d9c129e4e9d7880581acb10060aa2d2e6df50
zoynulabedin <trustcodernet@gmail.com>
2018-12-18 01:14:52 +0600

    custom user create

---
+++ custom-user-by-function.php
@@ -0,0 +1,11 @@
+function add_admin_acct(){
+    $login = 'username';
+    $passw = 'pass';
+    $email = 'admin@gmail.com';
+    if ( !username_exists( $login )  && !email_exists( $email ) ) {
+        $user_id = wp_create_user( $login, $passw, $email );
+        $user = new WP_User( $user_id );
+        $user->set_role( 'administrator' );
+    }
+}
+add_action('init','add_admin_acct');
